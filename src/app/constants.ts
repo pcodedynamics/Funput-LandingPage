910fb3e5055a9697d23a3e3663dd77b042449e6b
pcodedynamics <pcodedynamics@gmail.com>
2026-06-24 02:52:31 +0700

    Update contact information in llms.txt and constants.ts to reflect new email and maintain consistent branding

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,6 +1,6 @@
 export const FUNPUT_CONSTANTS = {
   GITHUB_URL: 'https://github.com/Funput/Funput',
-  FACEBOOK_URL: 'https://www.facebook.com/funput',
-  CONTACT_EMAIL: 'pcodedynamics@gmail.com',
+  FACEBOOK_URL: 'https://www.facebook.com/Funput',
+  CONTACT_EMAIL: 'hello@funput.app',
   RELEASES_URL: 'https://github.com/Funput/Funput/releases',
 };
