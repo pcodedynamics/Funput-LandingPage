edaf00a7a9723dfff5a724cb62fe6c006918d9db
pcodedynamics <pcodedynamics@gmail.com>
2026-06-24 22:12:35 +0700

    Replace old logo files with new SVG logo, update favicon references, and modify Facebook URL in relevant files for consistent branding.

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,6 +1,6 @@
 export const FUNPUT_CONSTANTS = {
   GITHUB_URL: 'https://github.com/Funput/Funput',
-  FACEBOOK_URL: 'https://www.facebook.com/Funput',
+  FACEBOOK_URL: 'https://www.facebook.com/FunputIME',
   CONTACT_EMAIL: 'hello@funput.app',
   RELEASES_URL: 'https://github.com/Funput/Funput/releases',
 };
